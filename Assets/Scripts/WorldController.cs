b77b93341590a6083854d3a1efcdfe1f23b0b8ac
Brayden Cooke <87751532+Cookiezznz@users.noreply.github.com>
2022-04-02 15:18:02 +1300

    Developer Scenes + Scripts
    Added scenes for each developer and some scripts for DodoController and WorldController

---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+    private Vector2 moveDirection = new Vector2(-0.5f, -0.5f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
