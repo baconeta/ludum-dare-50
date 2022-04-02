fc457bc891436f9a1725d6b630fc274001e33e48
Brayden Cooke <87751532+Cookiezznz@users.noreply.github.com>
2022-04-03 10:22:13 +1200

    FixedDodoPosition
    Dodo is now positioned correctly in the centre.

--- Assets/Scripts/Dodo.cs
+++ Assets/Scripts/Dodo.cs
@@ -6,15 +6,18 @@ using UnityEngine.UIElements;
 public class Dodo : MonoBehaviour
 {
     [SerializeField] private float dodoSpeed;
-    private float timer;
     private Vector3 dodoOrigin;
-    [SerializeField] private Transform cliffBounds;
-    [SerializeField] private Transform mountainBounds;
-    [SerializeField] private float behaviourChangeSpeed;
+    [Tooltip("How often the behaviour automatically changes")]
     private Vector3 m_ForwardDirection = new Vector3(1f, -.5f);
     private Vector3 m_CliffDirection = new Vector3(-.1f, -.05f);
     private Vector3 m_MountainDirection = new Vector3(.1f, .05f);
-    private int currentBehaviour;
+    [SerializeField] float sniffRange;
+    
+    //Movement and Behaviours
+    [SerializeField] private float behaviourChangeSpeed = 5;
+    private float _currentBehaviour = 1;
+    private bool _hasMoved = false;
+    
   
 
 
@@ -27,53 +30,80 @@ public class Dodo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MovePingPong();
-        if (Time.time % behaviourChangeSpeed + 0.05 > behaviourChangeSpeed)
-        {
-            currentBehaviour = Random.Range(1, 4);
-        }
-        if (currentBehaviour is 1)
-        {
-            
-        }
-        if (currentBehaviour is 2)
-        {
-            //MoveTowardCliff();
-        }
+        Move();
 
-        if (currentBehaviour is 3)
-        {
-            //MoveTowardMountain();
-        }
+    }
 
+    //Input desiredBehaviour to choose a behaviour
+    void ChangeMovementBehaviour(int desiredBehaviour = 0)
+    {
+        float timer = 1;//Time.time % Random.Range(4, 7);
+        if (desiredBehaviour != 0)
+        {
+            _currentBehaviour = desiredBehaviour;
+        }
+        else
+        {
+            if (timer < 0.1)
+            {
+                _currentBehaviour = Random.Range(1, 4);
+            }
+        }
         
-
-
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 
-            cliffBounds.position.x, 
-            mountainBounds.position.x),
-            Mathf.Clamp(transform.position.y,
-                cliffBounds.position.y,
-                mountainBounds.position.y));
-
+        
     }
 
-    void MoveTowardCliff()
+    void MoveForwards()
     {
-        Debug.Log("Move Cliff");
-        transform.position += m_CliffDirection * dodoSpeed;
-    }
-    
-    void MoveTowardMountain()
-    {
-        Debug.Log("Move Mountain");
-        transform.position += m_MountainDirection * dodoSpeed;
+        
     }
 
-    void MovePingPong()
+    void Move()
     {
-        transform.position = Vector3.Lerp(cliffBounds.position, mountainBounds.position,
-            Mathf.PingPong(Time.time / 100 * dodoSpeed, 1));
+        float timer = 1;//Time.time % behaviourChangeSpeed;
+
+        if (timer < 0.1 && !_hasMoved)
+        {
+            ChangeMovementBehaviour();
+            _hasMoved = true;
+        }
+        else if (timer > 0.1)
+        {
+            _hasMoved = false;
+        }
+        
+        switch (_currentBehaviour)
+        {
+            case 1:
+                //Debug.Log("Moving Forwards");
+                MoveForwards();
+                break;
+            case 2:
+                //Debug.Log("Moving Up");
+                MoveSinWave();
+                break;
+            case 3:
+                //Debug.Log("Moving Down");
+                MoveSinWave(true);
+                break;
+        }
+    }
+
+    //Direction: 1 = left, 2 = right
+    void MoveSinWave(bool moveDown = false)
+    {
+        float timer;
+        float sinWave = 0;
+        if (moveDown)
+        {
+            timer = 1;
+            sinWave = Mathf.Sin(timer);
+        }
+        if (sinWave > 0.99)
+        {
+            MoveForwards();
+        }
+        transform.position = dodoOrigin + (new Vector3(sinWave / 2, sinWave  / 4, 0) * dodoSpeed);
     }
 
 
