ce38e446debcc759e1288485c8484ac5481bccf6
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2022-04-02 22:31:41 +1300

    Integrate some props with stats

--- Assets/Scripts/Props/Bridge.cs
+++ Assets/Scripts/Props/Bridge.cs
@@ -2,6 +2,12 @@ namespace Props
 {
     public class Bridge : PlayerDraggable
     {
+        private StatsManager _statsManager;
+
+        private void Start()
+        {
+            _statsManager = FindObjectOfType<StatsManager>();
+        }
         
     }
 }
