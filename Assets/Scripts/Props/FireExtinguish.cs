05ca7af1ca439e3f2ac8b2eda8fdb0c797b0a06c
Tomas Morton <tomas.morton@hotmail.co.nz>
2022-04-02 15:29:21 -0400

    Update FireExtinguish to do something

--- Assets/Scripts/Props/FireExtinguish.cs
+++ Assets/Scripts/Props/FireExtinguish.cs
@@ -1,18 +1,13 @@
+using Props;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireExtinguish : MonoBehaviour
+//TODO: Change to PlayerScrubbable
+public class FireExtinguish : Smashable
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        base.Start();
     }
 }
