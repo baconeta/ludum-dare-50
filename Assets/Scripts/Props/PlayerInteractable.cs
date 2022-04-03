4ee02cb85f4e981ee3b1f3beaaccaddcc7e662df
Tomas Morton <tomas.morton@hotmail.co.nz>
2022-04-02 21:09:59 -0400

    Set IsDodoInteracting

--- Assets/Scripts/Props/PlayerInteractable.cs
+++ Assets/Scripts/Props/PlayerInteractable.cs
@@ -33,8 +33,9 @@ namespace Props
         {
         }
 
-        protected void DodoInteract()
+        public void DodoInteract(bool isInteracting)
         {
+            IsDodoInteracting = isInteracting;
         }
 
         /// <summary>
