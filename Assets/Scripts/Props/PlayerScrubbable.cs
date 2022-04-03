87665c294539474255967b3df7302000c23b0367
Brayden Cooke <87751532+Cookiezznz@users.noreply.github.com>
2022-04-04 11:09:41 +1200

    BushFires are now scrubbable
    Added PlayerScrubble as superclass of FireExtinguish.
    
    Fixed groundlarge prefab again

--- Assets/Scripts/Props/PlayerScrubbable.cs
+++ Assets/Scripts/Props/PlayerScrubbable.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Props;
 using UnityEngine;
 
-public class PlayerScrubbable : MonoBehaviour
+public class PlayerScrubbable : PlayerClickable
 {
-    // Start is called before the first frame update
-    void Start()
+    protected float scrubAmountRequired;
+    private float currentScrubAmount = 0;
+    private Vector3 previousMouseLocation;
+    private Vector3 mouseLocation;
+    protected override void OnMouseDown()
     {
-        
+        base.OnMouseDown();
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    protected void OnMouseDrag()
     {
-        
+        //Get distance of mouse movement between frames, then add that amount to currentScrubAmount.
+        previousMouseLocation = mouseLocation;
+        mouseLocation = ConvertMouseToWorldPosition(Input.mousePosition);
+        currentScrubAmount += Vector3.Distance(mouseLocation, previousMouseLocation);
+        Debug.Log(currentScrubAmount);
+        if (currentScrubAmount > scrubAmountRequired)
+        {
+            Destroy(this.gameObject);
+        }
     }
+    
 }
