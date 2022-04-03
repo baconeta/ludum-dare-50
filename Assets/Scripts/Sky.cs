318c801903d8ea66ef7f681a8bcd3fa1288dbefc
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2022-04-03 21:08:59 +1200

    Make sky speed scale with world speed

--- Assets/Scripts/Sky.cs
+++ Assets/Scripts/Sky.cs
@@ -1,34 +1,37 @@
+using Controllers;
 using UnityEngine;
 
 public class Sky : MonoBehaviour
 {
-
+    public WorldController worldController;
     public BoxCollider2D collider;
-
     public Rigidbody2D rb;
 
     [SerializeField] private Transform center;
-    private float Z;
+    [Tooltip("How much faster than the Dodo does the sky move.")]
+    [SerializeField] private float passiveSkySpeed = 0.5f;
 
+    private float originalZ;
     private float width;
 
-    private float scrollSpeed = -2f;
     
     void Start()
     {
+        worldController = FindObjectOfType<WorldController>();
         collider = GetComponent <BoxCollider2D>();
         rb = GetComponent <Rigidbody2D>();
 
         width = collider.size.x * transform.localScale.x;
         collider.enabled = false;
 
-        rb.velocity = new Vector2(scrollSpeed, 0);
-
-        Z = transform.position.z;
+        originalZ = transform.position.z;
     }
 
     void Update()
     {
+        float speed = worldController.getWorldSpeed() + passiveSkySpeed;
+        rb.velocity = new Vector2(-speed, 0);
+
         if (transform.position.x < center.position.x - width)
         {
             Vector3 resetAdjustment = new Vector3(width * 2f, 0, 0);
