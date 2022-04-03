c7d92937f6d0626b1a0e0426f265c75338a2cb63
Brayden Cooke <87751532+Cookiezznz@users.noreply.github.com>
2022-04-03 13:38:56 +1200

    added basic Dodo Movement with sway
    Dodo now moves across the terrain in a semi-random nature. Slight swaying and transitions help with removing snappiness

--- Assets/Scripts/Dodo.cs
+++ Assets/Scripts/Dodo.cs
@@ -6,45 +6,75 @@ using UnityEngine.UIElements;
 public class Dodo : MonoBehaviour
 {
     [SerializeField] private float dodoSpeed;
+    [SerializeField] private float dodoAcceleration;
+    private float _currentDodoAcceleration;
+    private Vector3 deccelerationOffset = new Vector3(0.5f, 0.25f);
+
     private Vector3 dodoOrigin;
     [Tooltip("How often the behaviour automatically changes")]
     private Vector3 m_ForwardDirection = new Vector3(1f, -.5f);
-    private Vector3 m_CliffDirection = new Vector3(-.1f, -.05f);
-    private Vector3 m_MountainDirection = new Vector3(.1f, .05f);
     [SerializeField] float sniffRange;
     
     //Movement and Behaviours
     [SerializeField] private float behaviourChangeSpeed = 5;
-    private float _currentBehaviour = 1;
+    private int _currentBehaviour = 1;
     private bool _hasMoved = false;
+    private float _behaviourTimer;
+
+    private bool b_isTransitionMovement;
+    [SerializeField]private float _wobbleWidth;
+    [SerializeField] private float _wobbleFrequency;
+ 
+
+
+    //Vector line that Dodo Moves on
+    private Vector3 _sideVector3 = new Vector3(.005f, 0.0025f);
     
+    //Boundaries
+    [SerializeField] Transform _cliffBound;
+    private Vector3 _cliffBoundPos;
+    [SerializeField] Transform _mountainBound;
+    private Vector3 _mountainBoundPos;
+
   
 
 
     // Start is called before the first frame update
     void Start()
     {
+        _cliffBoundPos = _cliffBound.position;
+        _mountainBoundPos = _mountainBound.position;
         dodoOrigin = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        _behaviourTimer = Time.time % behaviourChangeSpeed;
         Move();
-
+        Debug.Log(_currentDodoAcceleration);
     }
 
     //Input desiredBehaviour to choose a behaviour
     void ChangeMovementBehaviour(int desiredBehaviour = 0)
     {
-        float timer = 1;//Time.time % Random.Range(4, 7);
+        _currentDodoAcceleration = 0;
+        //If a chosen behaviour has been given
         if (desiredBehaviour != 0)
         {
             _currentBehaviour = desiredBehaviour;
         }
-        else
+        else //Get a new behaviour
         {
-            if (timer < 0.1)
+            //If current behaviour is Moving Forwards (1)
+            if (_currentBehaviour != 1)
+            {
+                // Then it is transitional movement - Will MoveForwards for 1 second.
+                _currentBehaviour = 1;
+                behaviourChangeSpeed = 1;
+                b_isTransitionMovement = true;
+            }
+            else 
             {
                 _currentBehaviour = Random.Range(1, 4);
             }
@@ -53,58 +83,82 @@ public class Dodo : MonoBehaviour
         
     }
 
-    void MoveForwards()
-    {
-        
-    }
 
     void Move()
     {
-        float timer = 1;//Time.time % behaviourChangeSpeed;
-
-        if (timer < 0.1 && !_hasMoved)
+        _currentDodoAcceleration += dodoAcceleration / 100;
+        _currentDodoAcceleration = Mathf.Clamp(_currentDodoAcceleration, 0, 1);
+        if (_behaviourTimer < 0.1 && !_hasMoved)
         {
             ChangeMovementBehaviour();
             _hasMoved = true;
+            
         }
-        else if (timer > 0.1)
+        else if (_behaviourTimer > 0.1)
         {
             _hasMoved = false;
+        }
+        else if (_behaviourTimer > behaviourChangeSpeed - 0.1)
+        {
+            b_isTransitionMovement = false;
+            behaviourChangeSpeed = 5;
         }
         
         switch (_currentBehaviour)
         {
+            //Move Forwards
             case 1:
-                //Debug.Log("Moving Forwards");
                 MoveForwards();
                 break;
+            //Move Towards Cliff
             case 2:
-                //Debug.Log("Moving Up");
-                MoveSinWave();
+                MoveTowardCliff();
                 break;
+            //Move Towards Mountain
             case 3:
-                //Debug.Log("Moving Down");
-                MoveSinWave(true);
+                MoveTowardMountain();
                 break;
+        }
+        //Current Acceleration %
+        _currentDodoAcceleration = Mathf.Clamp(_currentDodoAcceleration, 0, 1);
+    }
+    void MoveForwards()
+    {
+        float sinWave = _wobbleWidth * Mathf.Sin(Time.time * _wobbleFrequency);
+        transform.position += _sideVector3 * sinWave;
+
+    }
+    
+    void MoveTowardCliff()
+    { ;
+        if (transform.position.x <= _cliffBoundPos.x + deccelerationOffset.x)
+        {
+            _currentDodoAcceleration -= dodoAcceleration / 100;
+        }
+        transform.position +=  -_sideVector3 * dodoSpeed * _currentDodoAcceleration;
+        if (transform.position.x <= _cliffBoundPos.x)
+        {
+            ChangeMovementBehaviour(3);
+        }
+    }
+    
+    void MoveTowardMountain()
+    { ;
+        if (transform.position.x >= _mountainBoundPos.x - deccelerationOffset.x)
+        {
+            _currentDodoAcceleration -= dodoAcceleration / 100;
+            
+        }
+
+        transform.position += _sideVector3 * dodoSpeed * _currentDodoAcceleration;
+        if (transform.position.x >= _mountainBoundPos.x)
+        {
+            ChangeMovementBehaviour(2);
         }
     }
 
-    //Direction: 1 = left, 2 = right
-    void MoveSinWave(bool moveDown = false)
-    {
-        float timer;
-        float sinWave = 0;
-        if (moveDown)
-        {
-            timer = 1;
-            sinWave = Mathf.Sin(timer);
-        }
-        if (sinWave > 0.99)
-        {
-            MoveForwards();
-        }
-        transform.position = dodoOrigin + (new Vector3(sinWave / 2, sinWave  / 4, 0) * dodoSpeed);
-    }
+    
+    
 
 
 }
