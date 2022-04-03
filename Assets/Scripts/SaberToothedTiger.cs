4c2e30d026b026173a06469e5daf40a2c5b016ff
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-04-03 21:16:47 +0300

    tiger chasing code

--- Assets/Scripts/SaberToothedTiger.cs
+++ Assets/Scripts/SaberToothedTiger.cs
@@ -10,19 +10,47 @@ public class SaberToothedTiger : MonoBehaviour
 
     private WorldController _worldController;
 
+    private Dodo _dodoToChase;
+    private Vector3 _directionOfFocus;
+    [SerializeField] private float sidewaysSpeed = 2f;
+
+    //Vector line that Tiger will moves on
+    private Vector3 _sideVector3 = new Vector3(.5f, 0.25f) / 100;
+    [SerializeField] Vector3 tigerForwardVector = new Vector3(1f, -.5f);
+
     // Start is called before the first frame update
     private void Start()
     {
         _worldController = FindObjectOfType<WorldController>();
         // On spawn we want the tiger to start running towards the dodo.
         _fullSaberSpeed = _worldController.getWorldSpeed() + speedAboveWorld;
+        _dodoToChase = FindObjectOfType<Dodo>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        MoveTowardsDodo();
         transform.position +=
             _worldController.getMoveDirection() *
             (Time.deltaTime * (_fullSaberSpeed - _worldController.getWorldSpeed()));
+    }
+
+    void MoveTowardsDodo()
+    {
+        Vector3 currentPos = transform.position;
+        _directionOfFocus = currentPos - _dodoToChase.transform.position;
+
+        //Is smelled object to Dodo's left or right
+        float smellCrossProduct = Vector3.Cross(_directionOfFocus.normalized, tigerForwardVector.normalized).z;
+        //if CrossProduct is > 0, move towards mountains
+        if (smellCrossProduct > 0)
+        {
+            transform.position += _sideVector3 * sidewaysSpeed;
+        }
+        else if (smellCrossProduct < 0) //Move towards cliff
+        {
+            transform.position += -_sideVector3 * sidewaysSpeed;
+        }
     }
 }
