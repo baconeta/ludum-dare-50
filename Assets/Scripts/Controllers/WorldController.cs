9af3f153a05c5c72abce1f6970c8e553c2f1f049
Brayden Cooke <87751532+Cookiezznz@users.noreply.github.com>
2022-04-03 19:47:31 +1200

    Fix/World Controller setter

--- Assets/Scripts/Controllers/WorldController.cs
+++ Assets/Scripts/Controllers/WorldController.cs
@@ -66,7 +66,7 @@ namespace Controllers
             return baselineWorldSpeed * currentRampSpeedModifier * environmentalSpeedModifier;
         }
 
-        public float setWorldSpeed(float speed)
+        public void setWorldSpeed(float speed)
         {
             baselineWorldSpeed = speed;
         }
