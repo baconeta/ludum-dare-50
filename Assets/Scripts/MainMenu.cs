b40eea4e6f96a38f49fbeb5640ca3d88faea645a
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-04-04 13:11:56 +0300

    Basic Main menu functionality done

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string playGameScene;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(playGameScene);
+    }
+}
