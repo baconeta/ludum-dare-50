53e7dbb55328e4ad4ec93a15ef3d1a84cebfbff5
Brayden Cooke <87751532+Cookiezznz@users.noreply.github.com>
2022-04-05 01:14:12 +1200

    update from main

--- Assets/Scripts/Props/PlayerScrubbable.cs
+++ Assets/Scripts/Props/PlayerScrubbable.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using Props;
 using UnityEngine;
 
 public class PlayerScrubbable : PlayerClickable
 {
-    protected SpriteRenderer spriteRenderer;
-    protected Collider2D Collider;
-    protected Animator animator;
+
+    protected SpriteRenderer _spriteRenderer;
+    protected Collider2D _collider;
+    protected Animator _animator;
 
     protected float scrubAmountRequired;
     private float currentScrubAmount = 0;
     private Vector3 previousMouseLocation;
     private Vector3 mouseLocation;
     protected Sprite scrubbedSprite;
+    protected bool scrubbed = false;
 
     protected override void Start()
     {
         base.Start();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        Collider = GetComponent<Collider2D>();
-        animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<Collider2D>();
+        _animator = GetComponent<Animator>();
     }
 
     protected override void OnMouseDown()
@@ -36,9 +36,16 @@ public class PlayerScrubbable : PlayerClickable
         currentScrubAmount += Vector3.Distance(mouseLocation, previousMouseLocation);
         if (currentScrubAmount > scrubAmountRequired)
         {
-            spriteRenderer.sprite = scrubbedSprite;
-            Collider.enabled = false;
-            animator.enabled = false;
+            HandleScrub();
         }
     }
+
+    protected virtual void HandleScrub()
+    {
+        scrubbed = true;
+        _spriteRenderer.sprite = scrubbedSprite;
+        _collider.enabled = false;
+        _animator.enabled = false;
+    }
+
 }
