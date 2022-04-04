246e7ac6d36ccd345c52832be9466df4162ecef3
Tomas Morton <tomas.morton@hotmail.co.nz>
2022-04-03 20:00:24 -0400

    Fix spawning of obstacles

--- Assets/Scripts/Controllers/ObstacleController.cs
+++ Assets/Scripts/Controllers/ObstacleController.cs
@@ -9,6 +9,7 @@ namespace Controllers
     {
         [SerializeField] private GameObject[] standardHazards;
         [SerializeField] private GameObject[] largeHazards;
+        [SerializeField] private bool debug;
         private WorldController _worldController;
 
         private void Start()
@@ -18,9 +19,18 @@ namespace Controllers
 
         public void SpawnHazard(GameObject parent, float parentWidth, float parentHeight)
         {
-            var obstacleType = GetRandomHazardType(parent);
-            SpawnObstacle(obstacleType, parent, parentWidth, parentHeight);
-            SpawnHazardBypassIfExists(obstacleType, parent, parentWidth, parentHeight);
+            if (!debug)
+            {
+                var obstacleType = GetRandomHazardType(parent);
+                SpawnObstacle(obstacleType, parent, parentWidth, parentHeight);
+                SpawnHazardBypassIfExists(obstacleType, parent, parentWidth, parentHeight);
+            }
+            else
+            {
+                //Spawns an object at each of the maximums to check that they're generating within the boundary
+                var obstacleType = GetRandomHazardType(parent);
+                SpawnEdgeObstacles(obstacleType, parent, parentWidth, parentHeight);
+            }
         }
 
         private GameObject GetRandomHazardType(GameObject parent)
@@ -78,11 +88,17 @@ namespace Controllers
         {
             var newWidth = GetRandomValueFromMidpoint(width);
             var newHeight = GetRandomValueFromMidpoint(height);
-            var motion = -_worldController.getMoveDirection();
-            var framesMoved = Math.Abs(newWidth);
-            var adjustedHeight = newHeight + (motion * framesMoved).y;
+            return AdjustLocationForWorldMovement(newWidth, newHeight);
+        }
 
-            Debug.Log($"Offset for chunk of ({width}, {height}), randomised to ({newWidth}, {newHeight}). Moved {framesMoved} at {motion}, giving a Y movement of {(motion * framesMoved).y} and a final position of ({newWidth}, {adjustedHeight})");
+        private Vector3 AdjustLocationForWorldMovement(float newWidth, float newHeight)
+        {
+            var motion = -_worldController.getMoveDirection();
+            var framesMoved = newWidth;
+            var adjustedHeight = newHeight - (motion * framesMoved).y;
+
+            if (debug)
+                Debug.Log($"Offset for obstacle randomised to ({newWidth}, {newHeight}). Moved {framesMoved} at {motion}, giving a Y movement of {(motion * framesMoved).y} and a final position change of ({newWidth}, {adjustedHeight})");
 
             return new Vector3(newWidth, adjustedHeight, 0);
         }
@@ -92,5 +108,65 @@ namespace Controllers
             var maxOffset = size * padding / 2f;
             return Random.Range(-maxOffset, maxOffset);
         }
+
+        #region debug
+
+        private void SpawnEdgeObstacles(GameObject obstacleType, GameObject parent, float parentWidth, float parentHeight)
+        {
+            var obstacle = Instantiate(obstacleType, parent.transform);
+            var location = GetMaxMaxOffsetFromCenter(parentWidth, parentHeight);
+            obstacle.transform.position += location;
+
+            var obstacle2 = Instantiate(obstacleType, parent.transform);
+            var location2 = GetMaxMinOffsetFromCenter(parentWidth, parentHeight);
+            obstacle2.transform.position += location2;
+
+            var obstacle3 = Instantiate(obstacleType, parent.transform);
+            var location3 = GetMinMaxOffsetFromCenter(parentWidth, parentHeight);
+            obstacle3.transform.position += location3;
+
+            var obstacle4 = Instantiate(obstacleType, parent.transform);
+            var location4 = GetMinMinOffsetFromCenter(parentWidth, parentHeight);
+            obstacle4.transform.position += location4;
+        }
+
+        private Vector3 GetMaxMaxOffsetFromCenter(float width, float height)
+        {
+            var newWidth = GetMaxValueFromMidpoint(width);
+            var newHeight = GetMaxValueFromMidpoint(height);
+            return AdjustLocationForWorldMovement(newWidth, newHeight);
+        }
+
+        private Vector3 GetMaxMinOffsetFromCenter(float width, float height)
+        {
+            var newWidth = GetMaxValueFromMidpoint(width);
+            var newHeight = GetMinValueFromMidpoint(height);
+            return AdjustLocationForWorldMovement(newWidth, newHeight);
+        }
+
+        private Vector3 GetMinMaxOffsetFromCenter(float width, float height)
+        {
+            var newWidth = GetMinValueFromMidpoint(width);
+            var newHeight = GetMaxValueFromMidpoint(height);
+            return AdjustLocationForWorldMovement(newWidth, newHeight);
+        }
+
+        private Vector3 GetMinMinOffsetFromCenter(float width, float height)
+        {
+            var newWidth = GetMinValueFromMidpoint(width);
+            var newHeight = GetMinValueFromMidpoint(height);
+            return AdjustLocationForWorldMovement(newWidth, newHeight);
+        }
+
+        private static float GetMaxValueFromMidpoint(float size, float padding = 0.6f)
+        {
+            var maxOffset = size * padding / 2f;
+            return maxOffset;
+        }
+
+        private static float GetMinValueFromMidpoint(float size, float padding = 0.6f)
+            => -GetMaxValueFromMidpoint(size, padding);
+
+        #endregion debug
     }
 }
